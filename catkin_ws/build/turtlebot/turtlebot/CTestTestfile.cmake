e3170efd04c15c955b29403dc8d8ff31c4a2ff14
Pranali Desai <pranali.yashodhar@gmail.com>
2020-04-22 23:15:22 -0400

    turtlebot_simulator/

--- catkin_ws/build/turtlebot/turtlebot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sanket/Desktop/workspace/catkin_ws/src/turtlebot/turtlebot
-# Build directory: /home/sanket/Desktop/workspace/catkin_ws/build/turtlebot/turtlebot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sanket/Desktop/workspace/catkin_ws/src/world
-# Build directory: /home/sanket/Desktop/workspace/catkin_ws/build/world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
