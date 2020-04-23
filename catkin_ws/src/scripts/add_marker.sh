904beb2a8a266c3957cbe1c6c638c90e4138933f
Pranali Desai <pranali.yashodhar@gmail.com>
2020-04-23 01:29:27 -0400

    3 new files

---
+++ catkin_ws/src/scripts/add_marker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# export TURTLEBOT_GAZEBO_WORLD_FILE="$(rospack find world)/project1_world.world"
+# export TURTLEBOT_GAZEBO_MAP_FILE="$(rospack find world)/map2.yaml"
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e "rosrun add_markers add_markers" &
